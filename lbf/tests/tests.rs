f02fa9581bd147213de2279342a6aa1185c0b5ef
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2024-02-28 19:51:21 +0100

    tests fix

--- lbf/tests/tests.rs
+++ lbf/tests/tests.rs
@@ -2,14 +2,15 @@
 mod tests {
     use std::path::Path;
 
+    use rand::{Rng, SeedableRng};
     use rand::prelude::IteratorRandom;
     use rand::prelude::SmallRng;
-    use rand::{Rng, SeedableRng};
     use test_case::test_case;
 
     use jagua_rs::entities::problems::problem_generic::LayoutIndex;
     use jagua_rs::entities::problems::problem_generic::ProblemGeneric;
     use jagua_rs::io::parser::Parser;
+    use jagua_rs::util::polygon_simplification::PolySimplConfig;
     use lbf::io;
     use lbf::lbf_config::LBFConfig;
     use lbf::lbf_optimizer::LBFOptimizer;
@@ -30,9 +31,14 @@ mod tests {
         let instance = Path::new(instance_path);
         // parse the instance
         let mut config = LBFConfig::default();
-        config.n_samples_per_item = 100;
+        config.n_samples = 100;
         let json_instance = io::read_json_instance(&instance);
-        let parser = Parser::new(config.poly_simpl_config, config.cde_config, true);
+        let poly_simpl_config = match config.poly_simpl_tolerance {
+            Some(tolerance) => PolySimplConfig::Enabled { tolerance },
+            None => PolySimplConfig::Disabled,
+        };
+
+        let parser = Parser::new(poly_simpl_config, config.cde_config, true);
         let instance = parser.parse(&json_instance);
 
         let mut optimizer = LBFOptimizer::new(instance.clone(), config, SmallRng::seed_from_u64(0));
