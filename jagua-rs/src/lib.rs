596ff415419e64dff95dbea0654b5d8206bbfc17
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2024-03-21 13:59:43 +0100

    improved "double-precision" feature documentation

--- jagua-rs/src/lib.rs
+++ jagua-rs/src/lib.rs
@@ -2,9 +2,7 @@
 //! A fast and fearless Collision Detection Engine for 2D irregular cutting and packing problems.
 //!
 //!
-//! This crate can be configured to use single or double precision floating points:
-//! * By default, single precision is used: ```f32```
-//! * With feature ```double_precision```: ```f64```
+//! This crate can be configured to use single or double precision for floating points (see [fsize]).
 
 /// Everything collision detection engine related
 pub mod collision_detection;
@@ -23,11 +21,17 @@ pub mod util;
 
 /// The floating point type used in jagua-rs.
 cfg_if::cfg_if! {
-    if #[cfg(feature = "double_precision")] {
+    if #[cfg(feature = "double-precision")] {
+        /// The floating point type used in jagua-rs.
+        /// ```f32``` by default, ```f64``` with feature **double-precision**.
         pub type fsize = f64;
+        /// The constant PI as [fsize].
         pub const PI : fsize = std::f64::consts::PI;
     } else {
+        /// The floating point type used in jagua-rs.
+        /// ```f32``` by default, ```f64``` with feature **double-precision**.
         pub type fsize = f32;
+        /// The constant PI as [fsize].
         pub const PI: fsize = std::f32::consts::PI;
     }
 }
