3cffa901e6d6882e66b4e1ac5303df477f41cd67
dearfl <i@flr.me>
2025-04-25 20:49:15 +0800

    simplify map call by remove unnecessary closure

--- lbf/src/opt/lbf_opt_bpp.rs
+++ lbf/src/opt/lbf_opt_bpp.rs
@@ -101,7 +101,7 @@ fn search_layouts(
     sample_counter: &mut usize,
 ) -> Option<BPPlacement> {
     //search all existing layouts and closed bins with remaining stock
-    let open_layouts = problem.layouts.keys().map(|lk| BPLayoutType::Open(lk));
+    let open_layouts = problem.layouts.keys().map(BPLayoutType::Open);
     let bins_with_stock =
         problem
             .bin_qtys
