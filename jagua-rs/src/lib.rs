c9ffab9ba7e1ab9004919de5f33d40d234eb43d0
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-05-05 18:12:00 +0200

    docs

--- jagua-rs/src/lib.rs
+++ jagua-rs/src/lib.rs
@@ -2,10 +2,7 @@
 //! A fast and fearless Collision Detection Engine for 2D irregular cutting and packing problems.
 //!
 //! This library is designed to be used as a backend by optimization algorithms.
-//!
-//! All logic agnostic to a specific problem variant is implemented in the `jagua-rs-base` module.
-//! Specific problem variants can be added by enabling the corresponding feature flag in your `Cargo.toml`.
-//!
+
 #![doc = document_features::document_features!()]
 
 #[doc(inline)]
