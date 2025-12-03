65c6e6c231e1bfebba38448d1a82c0661e2d0a9f
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-12-03 22:09:13 +0100

    [wip] Mixed Strip Packing Problem

--- jagua-rs/src/probs/mspp/mod.rs
+++ jagua-rs/src/probs/mspp/mod.rs
@@ -1,3 +1,8 @@
-mod entities;
-mod io;
-mod util;
+/// Entities to model the Mixed Strip Packing Problem (MSPP)
+pub mod entities;
+
+/// Importing problem instances into and exporting solutions out of this library
+pub mod io;
+
+/// Helper functions which do not belong to any specific module
+pub mod util;
